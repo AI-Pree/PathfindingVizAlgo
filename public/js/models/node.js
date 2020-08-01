729f2e1cb9560e9c3d22f4a309b482e1a46e4500
Pree <preeyashp@gmail.com>
2020-08-01 22:30:45 +1000

    added comment for the node attributes

--- public/js/models/node.js
+++ public/js/models/node.js
@@ -1,18 +1,19 @@
 /**
  * @class Node
  * it creates a new nodes for the cell in the HTML table
+ * @param id
+ *  id of the node
+ * @param status
+ *  empty is the default node
+ *  occupied is the wall node created by the user
+ *  weighed is the node that has higher cost than other nodes
+ *  checkpoint is the node which needs to be visited before reaching the final destination
  */
 export default class Node{
     constructor(id, status){
         this.id = id;
-        /**
-         * @param status
-         * empty is the default node
-         * occupied is the wall node created by the user
-         * weighed is the node that has higher cost than other nodes
-         * checkpoint is the node which needs to be visited before reaching the final destination
-         */
-        this.status = status; // status is to determine what kind of node it is i.e empty, occupied, weighted (node that has higher cost than normal node) and checkpoint
+       
+        this.status = status; // status is to determine what kind of node it is i.e empty, occupied, weighted (node that has higher cost than normal node), visited and checkpoint
         this.weight = 0; 
     }
 
