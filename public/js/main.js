7e38b096d2428b74d87b26a6bc49e808529f5de8
Pree <preeyashp@gmail.com>
2020-08-01 19:18:39 +1000

    removed the exessive height in the table

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,35 @@
 import Board from './models/board.js';
 
-let height = Math.floor($(document).height()/25);
+
+
+let nav_height = document.getElementById("nav_html").clientHeight;
+/**
+ * @param height
+ * the number of rows in a table
+ * the height based on the window size of the browser and dividing it by 25 to assign the height of the cell size in the grid to be 25
+ * determinses how many rows can be added in the table
+ */
+let height = Math.floor(($(document).height() - nav_height)/25); // subtracting the document height with the nav height in the browser
+
+/**
+ * @param width
+ * the number of columns in a table
+ * the width based on the window size of the browser and dividing it by 25 to assign the width of the cell size in the grid to be 25px
+ * determines how many columns can be added in the table
+ */
 let width = Math.floor($(document).width()/25);
 
+/**
+ * @function initialise
+ * Initialises the whole board
+ * Creates a new board instance and creates the board to display in the browser
+ */
 var initialise = function(){
     let new_board = new Board(height, width);
     new_board.createBoard();
     console.log("Board of size " + new_board.height + "x" + new_board.width + " has been successfully created.....")
 }
-console.log(height, width);
+console.log("height: " + height, "width: " + width);
 console.log("Creating a new board......");
 initialise();
 
