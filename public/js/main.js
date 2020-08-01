ccf4523cff2728e0e19dcdc8c5643a21c7237f17
Pree <preeyashp@gmail.com>
2020-08-01 15:44:49 +1000

    fixed the js import to the index file

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,13 @@
-import board from './models/board.js';
+import Board from './models/board.js';
 
 let height = Math.floor($(document).height()/25);
 let width = Math.floor($(document).width()/25);
 
 var initialise = function(){
-    let new_board = new board(height, width)
+    let new_board = new Board(height, width);
     new_board.createBoard();
 }
-console.log("Creating a new board......")
+console.log(height, width);
+console.log("Creating a new board......");
 initialise();
 
