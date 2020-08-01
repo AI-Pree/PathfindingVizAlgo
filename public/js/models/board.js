794c12ec3947c7bb152ee31f69e68c4e00599ec2
Pree <preeyashp@gmail.com>
2020-08-02 00:15:59 +1000

    added getter and setters for the board class

--- public/js/models/board.js
+++ public/js/models/board.js
@@ -7,13 +7,54 @@ import Node from './node.js';
  * Node is identified with each cell in the grid
  */
 export default class Board{
+    //private fields
+    #height 
+    #width 
+    #nodes 
+    #grid 
+    #start
+    #destination
+
+    /**
+     * @constructor 
+     * @param height 
+     * determines the number of rows in the table
+     * @param width 
+     * determines the number of cols in the table
+     */
     constructor(height, width){
-        this.height = height;
-        this.width = width;
-        this.nodes = {}; // stores the nodes with its node id  as a key value pair where node is the value and node_id is the pair in a dict
-        this.grid = [];
-        this.start = ''; // holds the cell address of the start node in the table
-        this.destination = ''; // holds the cell address of the destination node in the table
+        this.#height = height;
+        this.#width = width;
+        this.#nodes = {}; // stores the nodes with its node id  as a key value pair where node is the value and node_id is the pair in a dict
+        this.#grid = [];
+        this.#start = ''; // holds the cell address of the start node in the table
+        this.#destination = ''; // holds the cell address of the destination node in the table
+    }
+
+    //getter and setters
+
+    get height(){
+        return this.#height;
+    }
+
+    set height(height){
+        this.#height = height;
+    }
+
+    get width(){
+        return this.#width;
+    }
+
+    set width(width){
+        this.#width = width;
+    }
+
+    get grid(){
+        return this.#grid;
+    }
+
+    set grid(grid){
+        this.#grid = grid;
     }
 
     /**
@@ -31,15 +72,14 @@ export default class Board{
                 let node_id = row + ":" + col, node_status, new_node;                
                 node_status = "unvisited";
                 new_node = new Node(node_id, node_status); //creating node for each cell in the grid
-                this.nodes[node_id]=new_node; // adding all the nodes to a dict for easier use of accessing the value
+                this.#nodes[node_id]=new_node; // adding all the nodes to a dict for easier use of accessing the value
                 curr_html_row += '<td id='+ '"' + node_id + '"' + " class= " + '"' + node_status +'"' + '></td>';
                 curr_row.push(new_node);
             };
-            this.grid.push(curr_row);
+            this.#grid.push(curr_row);
             html_grid += curr_html_row + '</tr>';
         };
-        var board = document.getElementById('board');
-        board.innerHTML = html_grid; // adding the grid inside the div tag with id board
+        document.getElementById('board').innerHTML = html_grid; // adding the grid inside the div tag to table with id board
         console.log("creating.....")
     }
 
@@ -51,8 +91,8 @@ export default class Board{
      */
     getNodes(node_id){
         let rowxcol = node_id.split(":");
-        let row = rowxcol[0];
-        let col = rowxcol[1];
-        return this.grid[row][col];
+        let row = parseInt(rowxcol[0]);
+        let col = parseInt(rowxcol[1]);
+        return this.#grid[row][col];
     }
 }
