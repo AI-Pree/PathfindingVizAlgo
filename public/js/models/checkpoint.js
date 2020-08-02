61607e69aecf4cf47c6c8e139241f5221d0bc906
Pree <preeyashp@gmail.com>
2020-08-02 13:16:14 +1000

    added models for the weight and checkpoint

---
+++ public/js/models/checkpoint.js
@@ -0,0 +1,8 @@
+/**
+ * @constructor checkpoint
+ * checkpoint is the node which should be visited by
+ * the algorithm before reaching the destination points
+ */
+export default checkpoint = (node_address) => {
+    this.node_address = node_address; // is the address of the node where checkpoint desired to be added
+}
---
+++ public/js/models/weight.js
@@ -0,0 +1,7 @@
+/**
+ * @constructor weight
+ * is the weight of the node
+ */
+export default weight = (cost) => {
+    this.cost = cost // cost of the node
+}
