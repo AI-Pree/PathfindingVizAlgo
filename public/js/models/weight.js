5045be85e389c2200980764507f40ab34cb30039
Pree <preeyashp@gmail.com>
2020-08-02 13:36:13 +1000

    added comment to weight

--- public/js/models/weight.js
+++ public/js/models/weight.js
@@ -1,6 +1,7 @@
 /**
  * @constructor weight
  * is the weight of the node
+ * Adding weights delays the algorithm time when it visits that particular weighted node
  */
 const Weight = (cost) => {
     this.cost = cost // cost of the node
