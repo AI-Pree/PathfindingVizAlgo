9f72876ab82f118288c4ceac67c580962815c76b
Pree <preeyashp@gmail.com>
2020-08-02 13:15:28 +1000

    added event listeners for the add, stop and clear buttons

--- public/js/controllers/eventController.js
+++ public/js/controllers/eventController.js
@@ -1,4 +1,5 @@
-
+import weight from 'models/weight.js'
+import checkpoint from 'models/checkpoint.js'
 /**
  * @function draw
  *  create a wall in the grid
@@ -7,7 +8,6 @@
  * 
  */
 export function draw(node_id){
-
     //create a new event for the draw
 
 
@@ -27,17 +27,24 @@ export var add = {
         /**
          * @function weight
          * Add weight to the node selected by the user in real-time
+         * creates a box where you can add the weights based on their priority level
          */
         weight:function(){
-
-            console.log("this function add weight in the cell");
+            let weight_el = document.getElementById("add_weight");
+            weight_el.addEventListener("click", () => {
+                console.log("this function add weight in the cell");
+            });
         },
         /**
          * @function checkpoint
          * Add checkpoint to the node selected by the user in real-time
+         * creates a window box where you can add the checkpoints based on their priority level
          */
         checkpoint:function(){
-            console.log("this function adds checkpoint in the cell")
+            let weight_el = document.getElementById("add_checkpoint");
+            weight_el.addEventListener("click", () => {
+                console.log("this function adds checkpoint in the cell")
+            });
         }
     }
 }
@@ -50,5 +57,27 @@ export function run(){
     const run_el = document.getElementById("run");
     run_el.addEventListener("click", ()=>{
         console.log("run")
+    })
+}
+
+/**
+ * @function stop
+ * stops the running algorithm when clicked on the stop button
+ */
+export var stop = () => { 
+    let stop_el = document.getElementById("stop");
+    stop_el.addEventListener("click",()=>{
+        console.log("stop")
+    })
+}
+
+/**
+ * @function clear
+ * clears all the wall, weights and checkpoint created by the user
+ */
+export var clear = () => {
+    let clear_el = document.getElementById("clear");
+    clear_el.addEventListener("click", () => {
+        console.log("clear")
     })
 }
