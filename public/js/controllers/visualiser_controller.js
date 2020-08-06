6a0024c3bfab57079f013eaf5bc7127482044b59
Pree <preeyashp@gmail.com>
2020-08-06 21:38:03 +1000

    git replace animation with visualiser controller

---
+++ public/js/controllers/visualiser_controller.js
@@ -0,0 +1,20 @@
+/**
+ * Visualiser Controller  
+ */
+
+ /**
+  * @constructor Visualiser
+  */
+export default function Visualiser(){}
+
+/**
+ * @function items_transition
+ * transition for adding of items in the cell
+ * @param cell 
+ * @param css 
+ */
+Visualiser.prototype.items_transition = function(cell, css){
+    cell.style.backgroundColor = css.backgroundColor || "";
+    cell.style.transitionDuration = css.transitionDuration || "0s";
+    cell.style.opacity = css.opacity || 1;
+} 
